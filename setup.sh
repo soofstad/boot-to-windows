e75048c3d9b5e2f001785520cd5297912410f3f1
Stig Ofstad <stigofstad@gmail.com>
2019-04-21 12:39:21 +0200

    fix: absolute path for icon

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cp $DIR/boot-to-windows.desktop ~/.local/share/applications
-sudo cp ./windows.png /usr/share/icons
+sudo cp $DIR/windows.png /usr/share/icons
 sudo cp -p $DIR/boot-to-windows.sh /usr/local/bin
