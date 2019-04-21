195cc42357d1b7bd786571d70a0f1df519a97f41
Stig Ofstad <stigofstad@gmail.com>
2019-04-21 12:33:13 +0200

    run setup for any directory

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
-cp ./boot-to-windows.desktop ~/.local/share/applications
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cp $DIR/boot-to-windows.desktop ~/.local/share/applications
 sudo cp ./windows.png /usr/share/icons
-sudo cp -p ./boot-to-windows.sh /usr/local/bin
+sudo cp -p $DIR/boot-to-windows.sh /usr/local/bin
